a2ad59045af51b266176a1cee7c5d18a88062c59
Alfie Driver <modernelm@hotmail.co.uk>
2014-10-26 18:35:05 +0000

    This includes some IF statements that will check if it is under a certain temperature. There is more coming along with a complete redesign

--- SkyFront/application.js
+++ SkyFront/application.js
@@ -11,7 +11,6 @@ function getAllDates($scope) {
 
 	//Getting the day
 	var day = new Date();
-	var newDay = day.getUTCDay();
 	var dayArray = new Array(7);
 	dayArray[0] = "Sunday";
 	dayArray[1] = "Monday";
@@ -20,22 +19,21 @@ function getAllDates($scope) {
 	dayArray[4] = "Thursday";
 	dayArray[5] = "Friday";
 	dayArray[6] = "Saturday";
-	var todayDay = dayArray[day.getDay()];
+	var todayDay = dayArray[day.getUTCDay()];
 	$scope.day = todayDay;
 
 	//getting the date
-	var dateOutput = document.getElementById('todaysDate');
 	var date = new Date();
 	var newDate = date.getDate();
-	$scope.date = newDate;6
+	$scope.date = newDate;
+
 	//getting the month
-	var monthOutput = document.getElementById('todaysMonth');
 	var month = new Date();
-	var newMonth = month.getMonth();
-	$scope.month = newMonth;
+	var newMonth = month.getUTCMonth();
+    var correctMonth = newMonth + 1;
+	$scope.month = correctMonth;
 
 	//getting the year
-	var yearOutput = document.getElementById('todaysYear');
 	var year = new Date();
 	var newYear = year.getFullYear();
 	$scope.year = newYear;
@@ -45,5 +43,21 @@ function getTemperature($scope){
 	//need something that will randomly generate a number between -5 and 25 degrees
 	var randomTemperature = Math.floor((Math.random() * 31) ); //range from 0 to 30
 	var finalTemperature = randomTemperature - 5; //minus 5 so that 0 turns into -5 and 30 turns into 25, the min and max range.
-	$scope.temperature = finalTemperature;
+	$scope.celcius = finalTemperature;
+
+    var convertToFahrenheit = (finalTemperature * 1.8) + 32;
+    var limitedFahrenheit = convertToFahrenheit.toFixed(1);
+    $scope.fahrenheit = limitedFahrenheit;
+
+    if (finalTemperature <= 5) {
+        $scope.warning = "CAUTION, it is less than 5 degrees outside";
+    }
+    if (finalTemperature <= 0){
+        $scope.warning = "&#9888;WARNING, ICE ALERT!";
+    }
+
+    if (finalTemperature >=20) {
+        $scope.information = "It is a nice day today";
+    }
+
 };
